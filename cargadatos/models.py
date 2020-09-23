4b7c547b5417109b8951c4449d4b59567995065c
NicolasGallardoAguilera <gallardo_yo@hotmail.com>
2020-09-23 16:30:47 -0300

    push

--- cargadatos/models.py
+++ cargadatos/models.py
@@ -7,36 +7,42 @@ class Profesor(models.Model):
     rut=models.CharField(max_length=20)
     nombre=models.CharField(max_length=20)
     apellido=models.CharField(max_length=20)
-    contraseña=models.CharField(max_length=20)
     Coordinador=models.BooleanField()
+    def __str__(self):
+        return self.nombre
 
 class Ramo(models.Model):
     CodigoRamo=models.CharField(max_length=20,primary_key=True)
     NombreRamo=models.CharField(max_length=20)
+    def __str__(self):
+        return self.NombreRamo
 
 class Seccion(models.Model):
     nrc=models.CharField(max_length=20,primary_key=True)
     semestre=models.CharField(max_length=20)
     CodigoRamo=models.ForeignKey(Ramo,on_delete=models.CASCADE)
     UsuarioUnab=models.ForeignKey(Profesor,on_delete=models.PROTECT)
-
-class Lista(models.Model):
-    nrc=models.ForeignKey(Seccion,on_delete=models.CASCADE)
-    UsuarioUnab=models.EmailField()
-    notas=models.DecimalField(decimal_places=3, max_digits=20)
-    estado=models.CharField(max_length=20)
-
+    def __str__(self):
+        return self.nrc
 
 class Estudiante (models.Model):
-    UsuarioUnab=models.EmailField(primary_key=True)
+    rut=models.CharField(primary_key=True,max_length=20)
     nombre=models.CharField(max_length=20)
     apellido=models.CharField(max_length=20)
-    contraseña=models.CharField(max_length=20)
-    rut=models.CharField(max_length=20)
-    nrc=models.ForeignKey(Lista,on_delete=models.PROTECT)
+    UsuarioUnab=models.EmailField(max_length=30)
     sede=models.CharField(max_length=20)
     carrera=models.CharField(max_length=50)
     jornada=models.CharField(max_length=20)
+    def __str__(self):
+        return str(self.UsuarioUnab)
+
+class Lista(models.Model):
+    nrc=models.ForeignKey(Seccion,on_delete=models.CASCADE)
+    rut=models.ForeignKey(Estudiante,on_delete=models.PROTECT)
+    notas=models.DecimalField(decimal_places=3, max_digits=20)
+    estado=models.CharField(max_length=20)
+    def __str__(self):
+        return self.nrc
 
 class Ejercicios(models.Model):
     IdEjercicio=models.CharField(max_length=20,primary_key=True)
@@ -51,14 +57,18 @@ class Ejercicios(models.Model):
     knowledge4=models.BooleanField()
     complejidad=models.CharField(max_length=20)
     hito=models.IntegerField()
+    def __str__(self):
+        return self.NombreProblema
 
 
 class TablonEjercicios(models.Model):
-    UsuarioUnab=models.ForeignKey(Estudiante,null=True,on_delete=models.PROTECT)
+    rut=models.ForeignKey(Estudiante,null=True,on_delete=models.CASCADE)
     dia=models.CharField(max_length=20)
     mes=models.CharField(max_length=20)
     año=models.CharField(max_length=20)
     IdEjercicio=models.ForeignKey(Ejercicios,on_delete=models.PROTECT)
     Puntaje=models.DecimalField(decimal_places=3, max_digits=20)
+    def __str__(self):
+        return str(self.rut)
 
     
--- cargadatos/views.py
+++ cargadatos/views.py
@@ -9,26 +9,28 @@ def index(request):
     return render(request,'cargadatos/index.html')
 
 
-class lista_tablon(ListView):
+class lista_tablon(TemplateView):
     model = TablonEjercicios
     template_name = 'cargadatos/TablonEjercicios.html'
 
  
-class vista_lista(LoginRequiredMixin,ListView):
+class vista_lista(LoginRequiredMixin,TemplateView):
     model= Lista
     queryset= Lista.objects.filter(nrc=2020)
     template_name = 'cargadatos/vista_lista.html'
 
-class vista_ejercicios_alumnos(LoginRequiredMixin,ListView):
+class vista_ejercicios_alumnos(LoginRequiredMixin,TemplateView):
     model= TablonEjercicios
-    queryset= TablonEjercicios.objects.filter(UsuarioUnab='nico')
+    queryset= TablonEjercicios.objects.filter(rut='nico')
     template_name = 'cargadatos/vista_ejercicios_alumnos.html'
 
-class listado_ejercicios(LoginRequiredMixin,  ListView):
+class listado_ejercicios(LoginRequiredMixin,TemplateView):
     model= Ejercicios
     template_name = 'cargadatos/listado_ejercicios.html'
 
 class vista_grafico(TemplateView):
     template_name = 'cargadatos/grafico.html'
+
+
 
 
